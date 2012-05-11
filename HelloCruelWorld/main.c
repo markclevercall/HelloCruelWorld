39e4a155299e09d00d06c1967d4c90f31b77961c
mark <mark@clevercall.ie>
2012-05-11 09:10:39 +0100

    Initial Commit

---
+++ HelloCruelWorld/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  HelloCruelWorld
+//
+//  Created by Mark Leddy on 11/05/2012.
+//  Copyright (c) 2012 CleverCall. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
